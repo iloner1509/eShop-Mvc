214a3defe10ffd84c957105eaa4fe462792fdea2
iloner1509@gmail.com <iloner1509@gmail.com>
2020-10-30 18:04:56 +0700

    Add project files.

---
+++ eShop-Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eShop_Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
